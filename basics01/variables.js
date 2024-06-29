ee7cb243546105803b589d48a7f4b231c6d83fc3
HARS07081811 <TRADEBYNEMWAL@GMAIL.COM>
2024-06-29 06:02:21 +0000

    Variables commite

---
+++ basics01/variables.js
@@ -0,0 +1,13 @@
+const accountid ="112233"
+let emailid ="nemwalhar@gmail.com"
+var accountpass = "887788"
+
+accountcity ="Delhi"
+
+// accountid = 2
+emailid = "har@gmail"
+accountpass="00000"
+accountcity="Jaipur"
+
+console.log(accountid);
+console.table([emailid, accountpass, accountcity]);
