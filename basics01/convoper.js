494fd8d99307b3e6f69274985d98767bdd223884
HARS07081811 <TRADEBYNEMWAL@GMAIL.COM>
2024-07-02 11:18:27 +0000

    ConversionOperation

---
+++ basics01/convoper.js
@@ -0,0 +1,26 @@
+let score = false
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+//"33abs" => NaN 
+// true => 1
+//false => 0
+// "33" => 33
+let IsloggedIn = 1
+
+let BooleanInLoggedIn = Boolean(IsloggedIn)
+console.log(BooleanInLoggedIn);
+
+//1 => true
+// Harsh => true
+// "" =>false
+
+let someNumber = 12
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber))
