cba17ab33d48b22872012c0e2ef9abbf33eb8f54
HARS07081811 <TRADEBYNEMWAL@GMAIL.COM>
2024-07-03 08:50:05 +0000

    operations

--- basics01/convoper.js
+++ basics01/convoper.js
@@ -1,26 +1,44 @@
-let score = false
-console.log(typeof score);
-console.log(typeof (score));
+// let score = false
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let ValueInNumber = Number(score)
-console.log(typeof ValueInNumber);
-console.log(ValueInNumber);
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
 
-//"33abs" => NaN 
-// true => 1
-//false => 0
-// "33" => 33
-let IsloggedIn = 1
+// //"33abs" => NaN 
+// // true => 1
+// //false => 0
+// // "33" => 33
+// let IsloggedIn = 1
 
-let BooleanInLoggedIn = Boolean(IsloggedIn)
-console.log(BooleanInLoggedIn);
+// let BooleanInLoggedIn = Boolean(IsloggedIn)
+// console.log(BooleanInLoggedIn);
 
-//1 => true
-// Harsh => true
-// "" =>false
+// //1 => true
+// // Harsh => true
+// // "" =>false
 
-let someNumber = 12
+// let someNumber = 12
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof (stringNumber))
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof (stringNumber))
+
+
+//**********Operations***********/
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hey"
+let str2 ="Harsh"
+let str3 = str1+str2
+console.log(str3);
+
+console.log(2+2);
+console.log(2*2);
+console.log(2-3);
+console.log(2%4);
+
